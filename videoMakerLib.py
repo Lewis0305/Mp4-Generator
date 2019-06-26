03972d15c50ab4407b94468695fb05eedf408d2b
Sam Lewis <lewiss4@oregonstate.edu>
2019-06-25 21:15:24 -0700

    Changed concat method (untested)

--- videoMakerLib.py
+++ videoMakerLib.py
@@ -45,22 +45,18 @@ def mp4_concatenation(clips, ind): # directory
 # stitch all mp4's togehter
 # THIS IS NOW PROVEN TO BE WHERE THE AUDIO GLICH IS FROM
 def final_concatenation(clips,ind):
-    vids = ""
-    n=1
-    #os.system('ffmpeg -i blank_slate/sta2.mp4 blank_slate/sta.mp4 -hide_banner')
-    os.system('ffmpeg -i workspace/blank_slate/New.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts workspace/inter/0.ts')
-    for i in clips:
-        os.system('ffmpeg -i workspace/Done_mp4/'+str(n)+'.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts workspace/inter/'+str(n)+'.ts')
-        if n > 1:
-            #vids += "|"
-            vids += "|workspace/inter/0.ts|"
-        vids += "workspace/inter/"+str(n)+'.ts'
-        n += 1
-    print(vids)
-    os.system('ffmpeg -i "concat:'+vids+'" -c copy -bsf:a aac_adtstoasc workspace/video/'+str(ind+1)+'.mp4')
-    for i in range(len(clips)):
-        os.remove(r'C:/Desk/Mp4Gen/workspace/Done_mp4' + '/' + str(i+1) + '.mp4')
-
+    names = '' # Names of all input mp4
+    clip = '' # Video and audio streams
+    total = 0  # Total number of clips
+    for n in clips:
+        names += '-i workspace/Done_mp4/' + str(total + 1) + '.mp4 '
+        names += '-i workspace/blank_slate/New.mp4 '
+        clip += '[' + str(total) + ':v:0][' + str(total) + ':a:0]'
+        clip += '[' + str(total+1) + ':v:0][' + str(total+1) + ':a:0]'
+        total += 2
+    os.system('ffmpeg ' + names + ' \
+              -filter_complex "' + clip + 'concat=n=' + str(total) + ':v=1:a=1[outv][outa]" \
+              -map "[outv]" -map "[outa]" workspace/video/' + str(ind+1) + '.mp4')
 
 """
 def final_concatenation(clips,ind):
