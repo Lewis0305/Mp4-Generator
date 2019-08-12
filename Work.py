a4b57210433056bf199d95f38127095835c752a2
Lewis0305 <Lewis1721.sl@gmail.com>
2019-08-12 01:20:33 -0700

    Paint Issue

--- Work.py
+++ Work.py
@@ -48,12 +48,43 @@ for title in images:
 
 
 
+        image.save('C:\\Users\\Sam\\Box Sync\\V\\Out\\'+str(dirc)+'\\'+str(i+1)+'-0.png')
+        name = 1
+        for dot in red_dots:
+                if [dot[0]-1, dot[1]-1] not in red_dots and \
+                   [dot[0], dot[1]-1] not in red_dots and \
+                   [dot[0]+1, dot[1]-1] not in red_dots:
+                        temp = image
+                        floor = 0
+                        for bot in wht_bttms:
+                                if dot[1] < bot:
+                                        floor = bot
+                                        break
+                        
+                        
+                        
+                        
+                        y_paint = 0
+                        print(floor)
+                        for y in image_array:
+                                x_paint = 0
+                                for xy in image_array:
+                                        if y_paint > floor+3:
+                                                temp.putpixel((x_paint,y_paint), (26 ,26 ,27))
+                                        if y_paint > dot[1] and x_paint > dot[0]:
+                                                temp.putpixel((x_paint,y_paint), (26 ,26 ,27))
+                                        x_paint += 1
+                                y_paint += 1
+                        temp.save('C:\\Users\\Sam\\Box Sync\\V\\Out\\'+str(dirc)+'\\'+str(i+1)+'-'+str(name)+'.png')
+                        name += 1
+        
 
 
-                                
+
+        
 
 
-        image.save('C:\\Users\\Sam\\Box Sync\\V\\Out\\'+str(dirc)+'\\'+str(i)+'.png')
+        #image.save('C:\\Users\\Sam\\Box Sync\\V\\Out\\'+str(dirc)+'\\'+str(i)+'-1.png')
 
 
 
